83aac47992835423f5fd40f2aa535816288e8866
Hao Phan <hao@addsearch.com>
2025-03-06 18:33:40 +0200

    Migrate from react-scripts (not recommended by React dev) to Vite

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -15,5 +12,4 @@ root.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
