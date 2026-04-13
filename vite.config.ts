89ec1c8aef6e218ae359b97acd65a0a508b3ec9e
Hao Phan <hao@addsearch.com>
2026-04-13 13:28:24 +0300

    [sc-13920] patch vite, vitest, vitejs/plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
